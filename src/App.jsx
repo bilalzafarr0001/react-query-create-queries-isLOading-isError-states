6af63f8e47fd403751c4140d3d3baccfc4c08a7e
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2023-01-27 17:10:31 +0500

    Add isLOading and isError state using useQuery in react-query using devtools for monitoring data

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Posts } from "./Posts";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "./App.css";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    // provide React Query client to App
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
