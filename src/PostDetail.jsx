e341d079f60f843fd0aef1f63e38d0ba332405b5
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2023-01-27 23:04:31 +0500

    Add update and delete fake uisng useMutation in react-query

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery , useMutation } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -30,6 +30,9 @@ export function PostDetail({ post }) {
     () => fetchComments(post.id),
     { cacheTime: 5000 }
   );
+  
+   const deleteMutation =  useMutation((postId)=> deletePost(postId))
+   const updateMutation =  useMutation((postId)=> updatePost(postId))
 
   if (isLoading) {
     return <h1>Loading ....</h1>;
@@ -47,7 +50,16 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={()=> deleteMutation.mutate(post.id)}> Delete</button>
+      {deleteMutation.isError && (<p style={{color:'red'}}>Error deleting post </p>)}
+      {deleteMutation.isLoading && (<p style={{color:'purple'}}>Deleting the post </p>)}
+      {deleteMutation.isSuccess && (<p style={{color:'green'}}>Post has (not) been deleted </p>)}
+
+      {updateMutation.isError && (<p style={{color:'red'}}>Error updating post </p>)}
+      {updateMutation.isLoading && (<p style={{color:'purple'}}>Updating the post </p>)}
+      {updateMutation.isSuccess && (<p style={{color:'green'}}>Post has (not) been updated </p>)}
+
+       <button onClick={()=> updateMutation.mutate(post.id)}>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
